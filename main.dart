3f63fec5d2766e6d639d8b89a145fd35e4a1a0a7
mohammedek <74847598+mohammedek@users.noreply.github.com>
2022-07-18 01:54:28 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:contacts_app/pages/contacts_page.dart';
+
+void main() {
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ContactPage(),
+    );
+  }
+}
+
+
+
